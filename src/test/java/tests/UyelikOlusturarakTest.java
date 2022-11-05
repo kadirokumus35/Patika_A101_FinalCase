bd713bdeeee168e144ef3d1c2f697c9417b1ee66
kadir <kdrokumus35@gmail.com>
2022-11-05 18:32:14 +0300

    biterken

--- src/test/java/tests/UyelikOlusturarakTest.java
+++ src/test/java/tests/UyelikOlusturarakTest.java
@@ -31,7 +31,7 @@ Log log;
 
         hepsiBurada.login.click();
 
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         hepsiBurada.kullaniciMailAdresi.sendKeys(ConfigReader.getProperty("email"));
         hepsiBurada.kullaniciMailSonrasiGirisYap.click();
         hepsiBurada.kullaniciSifre.sendKeys(ConfigReader.getProperty("sifre"));
@@ -44,9 +44,9 @@ Log log;
         hepsiBurada.aramaCubugu.sendKeys(ConfigReader.getProperty("arananUrun"));
         hepsiBurada.aramaButonu.click();
 
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         hepsiBurada.motorolaBebekTelsizi.click();
-        Thread.sleep(1000);
+        Thread.sleep(2000);
 
        String ilkSayfaWindowHandle=Driver.getDriver().getWindowHandle();
        Set<String> windowHandleSeti=Driver.getDriver().getWindowHandles();
@@ -60,11 +60,11 @@ Log log;
        }
        Driver.getDriver().switchTo().window(ikinciSayfaWHDegeri);
 
-        Thread.sleep(1000);
+        Thread.sleep(2000);
 
 
             hepsiBurada.sepeteEkle.click();
-            Thread.sleep(1000);
+            Thread.sleep(2000);
 
             hepsiBurada.alisveriseDevamEt.click();
 
@@ -72,7 +72,7 @@ Log log;
             hepsiBurada.aramaButonu.click();
             Thread.sleep(2000);
             hepsiBurada.motorolaBebekTelsizi2.click();
-            Thread.sleep(1000);
+            Thread.sleep(2000);
         String yeniSayfaWindowHandle=Driver.getDriver().getWindowHandle();
         Set<String> yeniwindowHandleSeti=Driver.getDriver().getWindowHandles();
 
@@ -85,12 +85,12 @@ Log log;
         }
         Driver.getDriver().switchTo().window(yeniikinciSayfaWHDegeri);
 
-        Thread.sleep(1000);
+        Thread.sleep(2000);
             hepsiBurada.sepeteEkle.click();
-            Thread.sleep(1000);
+            Thread.sleep(2000);
 
             hepsiBurada.sepeteGit.click();
-            Thread.sleep(1000);
+            Thread.sleep(2000);
 
         System.out.println(Driver.getDriver().getTitle());
         String sepetimSayfasindayiz=Driver.getDriver().getTitle();
